171b40392a5e449c4f756067fc276caa2020f7c8
Razie Fathi <raziefa@buffalo.edu>
2016-07-31 12:18:27 -0400

    Add Convex Hull

--- HighwayDataExplorer/tmjsfuncs.js
+++ HighwayDataExplorer/tmjsfuncs.js
@@ -774,6 +774,7 @@ function startEdgeSearch() {
 		connections[i].setMap(null);
 	}
 	//we don't need waypoints table here, so we remove those
+	var Table = document.getElementById("waypoints");
 	Table.innerHTML = "";
 	statusLine.innerHTML = 'Checking: <span style="color:yellow">0</span>';
 	setTimeout(continueEdgeSearch, delay,0,0);
@@ -1063,8 +1064,6 @@ function processQueue() {
 		// being here means that we didn't find any adjacent of top node that is not visited,
 		// so we are done with the top node, and we have to remove it from the top of stack, and coninute processing the stack.
 
-
-
 		stack.pop();
 		printList(stack);
 		setTimeout(processStack, delay);
@@ -1081,6 +1080,162 @@ function processQueue() {
 		setTimeout(processStack, delay);
 	}
 	}
+
+	function startConvexHall(){
+		calculateConvexHull();
+		//setTimeout(calculateConvexHull, delay);
+		// console.log("caculate convex hull " +  calculateConvexHull);
+	}
+
+
+var polyline;
+
+	function calculateConvexHull() {
+	     if (polyline) polyline.setMap(null);
+	   	 	p = [];
+			for (var i=0; i < markers.length; i++) {
+	    	p.push(markers[i].getPosition());
+	  }
+	      p.sort(sortPointY);
+	      p.sort(sortPointX);
+
+	    //  DrawHull();
+				setTimeout(DrawHull, delay);
+	}
+
+	function DrawHull() {
+	     hullPoints = [];
+	     chainHull_2D(p, p.length, hullPoints );
+	      polyline = new google.maps.Polygon({
+	      map: map,
+	      paths:hullPoints,
+	      fillColor:"#FF0000",
+	      strokeWidth:2,
+	      fillOpacity:0.5,
+	      strokeColor:"#0000FF",
+	      strokeOpacity:0.5
+	     });
+	}
+
+	function sortPointX(a, b) {
+	    return a.lng() - b.lng();
+	}
+	function sortPointY(a, b) {
+	    return a.lat() - b.lat();
+	}
+
+	function isLeft(P0, P1, P2) {
+	    return (P1.lng() - P0.lng()) * (P2.lat() - P0.lat()) - (P2.lng() - P0.lng()) * (P1.lat() - P0.lat());
+	}
+	//===================================================================
+	// Copyright 2001, softSurfer (www.softsurfer.com)
+	// This code may be freely used and modified for any purpose
+	// providing that this copyright notice is included with it.
+
+	// chainHull_2D(): A.M. Andrew's monotone chain 2D convex hull algorithm
+	// http://softsurfer.com/Archive/algorithm_0109/algorithm_0109.htm
+	//
+	//     Input:  P[] = an array of 2D points
+	//                   presorted by increasing x- and y-coordinates
+	//             n = the number of points in P[]
+	//     Output: H[] = an array of the convex hull vertices (max is n)
+	//     Return: the number of points in H[]
+
+
+	function chainHull_2D(P, n, H) {
+	    // the output array H[] will be used as the stack
+	    var bot = 0,
+	    top = (-1); // indices for bottom and top of the stack
+	    var i; // array scan index
+	    // Get the indices of points with min x-coord and min|max y-coord
+	    var minmin = 0,
+	        minmax;
+
+	    var xmin = P[0].lng();
+	    for (i = 1; i < n; i++) {
+	        if (P[i].lng() != xmin) {
+	            break;
+	        }
+	    }
+
+	    minmax = i - 1;
+	    if (minmax == n - 1) { // degenerate case: all x-coords == xmin
+	        H[++top] = P[minmin];
+	        if (P[minmax].lat() != P[minmin].lat()) // a nontrivial segment
+	            H[++top] = P[minmax];
+	        H[++top] = P[minmin]; // add polygon endpoint
+	        return top + 1;
+	    }
+
+	    // Get the indices of points with max x-coord and min|max y-coord
+	    var maxmin, maxmax = n - 1;
+	    var xmax = P[n - 1].lng();
+	    for (i = n - 2; i >= 0; i--) {
+	        if (P[i].lng() != xmax) {
+	            break;
+	        }
+	    }
+	    maxmin = i + 1;
+
+	    // Compute the lower hull on the stack H
+	    H[++top] = P[minmin]; // push minmin point onto stack
+	    i = minmax;
+	    while (++i <= maxmin) {
+	        // the lower line joins P[minmin] with P[maxmin]
+	        if (isLeft(P[minmin], P[maxmin], P[i]) >= 0 && i < maxmin) {
+	            continue; // ignore P[i] above or on the lower line
+	        }
+
+	        while (top > 0) { // there are at least 2 points on the stack
+	            // test if P[i] is left of the line at the stack top
+	            if (isLeft(H[top - 1], H[top], P[i]) > 0) {
+	                break; // P[i] is a new hull vertex
+	            }
+	            else {
+	                top--; // pop top point off stack
+	            }
+	        }
+
+	        H[++top] = P[i]; // push P[i] onto stack
+	    }
+
+	    // Next, compute the upper hull on the stack H above the bottom hull
+	    if (maxmax != maxmin) { // if distinct xmax points
+	        H[++top] = P[maxmax]; // push maxmax point onto stack
+	    }
+
+	    bot = top; // the bottom point of the upper hull stack
+	    i = maxmin;
+	    while (--i >= minmax) {
+	        // the upper line joins P[maxmax] with P[minmax]
+	        if (isLeft(P[maxmax], P[minmax], P[i]) >= 0 && i > minmax) {
+	            continue; // ignore P[i] below or on the upper line
+	        }
+
+	        while (top > bot) { // at least 2 points on the upper stack
+	            // test if P[i] is left of the line at the stack top
+	            if (isLeft(H[top - 1], H[top], P[i]) > 0) {
+	                break;  // P[i] is a new hull vertex
+	            }
+	            else {
+	                top--; // pop top point off stack
+	            }
+	        }
+
+	        H[++top] = P[i]; // push P[i] onto stack
+	    }
+
+	    if (minmax != minmin) {
+	        H[++top] = P[minmin]; // push joining endpoint onto stack
+	    }
+
+	    return top + 1;
+	}
+	function getObj(elementID){
+					return document.getElementById(elementID);
+				}
+
+
 
 	// JS debug window by Mike Maddox from
 	// http://javascript-today.blogspot.com/2008/07/how-about-quick-debug-output-window.html
