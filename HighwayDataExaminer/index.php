2e15f278fcaa8103423bafb0217582f8754a987a
Jim Teresco <terescoj@gmail.com>
2018-08-04 14:24:15 -0400

    Minor tweaks to UI.

--- HighwayDataExaminer/index.php
+++ HighwayDataExaminer/index.php
@@ -388,7 +388,7 @@ tr.pseudocode {
 	  </select>
 	</td>
 	<td>
-	  <input id="pseudoCheckbox" type="checkbox" name="Show selected algorithm pseudocode" onclick="showHidePseudocode()" >&nbsp;Pseudocode<br>
+	  <input id="pseudoCheckbox" type="checkbox" name="Pseudocode-level AV" onclick="showHidePseudocode()" >&nbsp;Trace Pseudocode<br>
 	</td>
 	<td>
 	  <input id="resetButton" type="button" value="Reset AV" onClick="clearForm(this.form)"/>
